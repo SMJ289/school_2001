bdee4dac0044e38998e404a5eafdbcf843a96a75
Steven Meyers <sjjmeyers@gmail.com>
2020-01-29 14:03:00 -0700

    Try tests in iteration 2

--- lib/school.rb
+++ lib/school.rb
@@ -1,5 +1,5 @@
 class School
-  attr_reader :start_time, :hours_in_school_day, :student_names
+  attr_reader :start_time, :hours_in_school_day, :student_names, :end_time
 
   def initialize(start_time, hours_in_school_day, student_names = [])
     @start_time = start_time
@@ -7,4 +7,13 @@ class School
     @student_names = student_names
   end
 
+  def add_student_name(student)
+    @student_names << student
+  end
+
+  def calculate_end_time
+    @end_time =
+
+
+  end
 end
